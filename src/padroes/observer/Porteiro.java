187136751bad222eb4ed210e43375b6d2cb0d6b9
rbverissimo <rverissimo.dev@gmail.com>
2022-12-16 09:57:44 -0300

    primeira implementação completa do padrão Observer

--- src/padroes/observer/Porteiro.java
+++ src/padroes/observer/Porteiro.java
@@ -1,7 +1,9 @@
 package padroes.observer;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Porteiro {
 	
@@ -13,5 +15,27 @@ public class Porteiro {
 	}
 	
 	
+	// esse código é implementado pelos frameworks, geralmente: 
+	public void monitorar() {
+		Scanner entrada = new Scanner(System.in);
+		
+		String valor = "";
+		while(!"sair".equalsIgnoreCase(valor)) {
+			System.out.println("Aniversariante chegou? ");
+			valor = entrada.nextLine();
+			if("sim".equalsIgnoreCase(valor)) {
+				// Criar o evento que será notificado para cada observador
+				EventoChegadaAniversariante evento =
+						new EventoChegadaAniversariante(new Date());
+				// notificar os observadores
+				observadores.stream()
+							.forEach(o -> o.chegou(evento));
+			} else System.out.println("Alarme falso");
+			
+		}
+		
+		entrada.close();
+		
+	}
 
 }
