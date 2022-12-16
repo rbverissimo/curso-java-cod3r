882e1bfcafc8ed2cb75c4c19a575f532332f5e9f
rbverissimo <rverissimo.dev@gmail.com>
2022-12-16 10:04:10 -0300

    usando as duas implementações: com a instância de Observador e com a interface funcional

--- src/padroes/observer/AniversarioSurpresa.java
+++ src/padroes/observer/AniversarioSurpresa.java
@@ -4,10 +4,14 @@ public class AniversarioSurpresa {
 	
 	public static void main(String[] args) {
 		
-		// Namorada namorada = new Namorada();
+		
+		// é possível instanciar o observador ou usar a interface funcional para isso
+		Namorada namorada = new Namorada();
 		Porteiro porteiro = new Porteiro();
 		
-		// porteiro.registrarObservador(namorada);
+		porteiro.registrarObservador(namorada);
+		
+		// registrando o Observador através do uso da interface funcional
 		porteiro.registrarObservador(e -> {
 			System.out.println("Surpresa via lambda!!!");
 			System.out.println("Ocorreu em: " + e.getMomento());	
