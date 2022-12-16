02d1c767ecd79b6a7aaef0f440bb5917c416a6ca
rbverissimo <rverissimo.dev@gmail.com>
2022-12-16 10:02:52 -0300

    implementando o mesmo código usando uma interface funcional

--- src/padroes/observer/AniversarioSurpresa.java
+++ src/padroes/observer/AniversarioSurpresa.java
@@ -3,10 +3,16 @@ package padroes.observer;
 public class AniversarioSurpresa {
 	
 	public static void main(String[] args) {
-		Namorada namorada = new Namorada();
+		
+		// Namorada namorada = new Namorada();
 		Porteiro porteiro = new Porteiro();
 		
-		porteiro.registrarObservador(namorada);
+		// porteiro.registrarObservador(namorada);
+		porteiro.registrarObservador(e -> {
+			System.out.println("Surpresa via lambda!!!");
+			System.out.println("Ocorreu em: " + e.getMomento());	
+		});
+		
 		porteiro.monitorar();
 	}
 
